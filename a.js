bdbd0acd72fcdccff2295069719480b6a82acdda
qishushi93 <56948186+qishushi93@users.noreply.github.com>
2020-06-07 19:37:53 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,21 @@
-# qishushi93.github.io
+      function getInputVal2(){
+				var inputVal = myForm.inputBox2.value;
+				var b="http://api.baiyug.vip/index.php?url=";
+				var c=b+inputVal;
+				if(inputVal){
+					window.open(c);
+				}else{
+					alert('请在输入框中填点东西');
+				}
+			}
+			function getInputVal(){
+				var inputVal = myForm.inputBox.value;
+				var b="https://jx.hh66.xyz/jiexi/?url=";
+				var c=b+inputVal;
+				if(inputVal){
+					window.open(c);
+				}else{
+					alert('请在输入框中填点东西');
+				}
+			}
+			
